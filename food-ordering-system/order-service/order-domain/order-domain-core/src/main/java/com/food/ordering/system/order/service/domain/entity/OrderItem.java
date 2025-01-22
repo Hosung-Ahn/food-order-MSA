e7d5a6a2849f5d52504e9d3c413d80b1b17648ea
Hosung <an3735297@naver.com>
2025-01-22 19:20:25 +0900

    주문 도메인 모델 설계

---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -0,0 +1,50 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
+
+public class OrderItem extends BaseEntity<OrderItemId> {
+    private OrderId orderId;
+    private final Product product;
+    private final int quantity;
+    private final Money subTotal;
+
+    private OrderItem(Builder builder) {
+        super.setId(builder.orderItemId);
+        orderId = builder.orderId;
+        product = builder.product;
+        quantity = builder.quantity;
+        subTotal = builder.subTotal;
+    }
+
+
+    public static final class Builder {
+        private OrderItemId orderItemId;
+        private OrderId orderId;
+        private final Product product;
+        private final int quantity;
+        private final Money subTotal;
+
+        public Builder(Product product, int quantity, Money subTotal) {
+            this.product = product;
+            this.quantity = quantity;
+            this.subTotal = subTotal;
+        }
+
+        public Builder orderItemId(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public Builder orderId(OrderId val) {
+            orderId = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
+}

cb2e07e11a806609de94257be92641db0f1aa4db
Hosung <an3735297@naver.com>
2025-01-22 20:57:28 +0900

    Product 빌더 패턴 추가

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -8,11 +8,10 @@ public class Product extends BaseEntity<ProductId> {
     private String name;
     private Money price;
 
-
-    public Product(ProductId productId, String name, Money price) {
-        super.setId(productId);
-        this.name = name;
-        this.price = price;
+    private Product(Builder builder) {
+        super.setId(builder.productId);
+        name = builder.name;
+        price = builder.price;
     }
 
     public String getName() {
@@ -21,5 +20,33 @@ public class Product extends BaseEntity<ProductId> {
 
     public Money getPrice() {
         return price;
+    }
+
+    public static final class Builder {
+        private ProductId productId;
+        private String name;
+        private Money price;
+
+        public Builder() {
+        }
+
+        public Builder id(ProductId val) {
+            productId = val;
+            return this;
+        }
+
+        public Builder name(String val) {
+            name = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Product build() {
+            return new Product(this);
+        }
     }
 }

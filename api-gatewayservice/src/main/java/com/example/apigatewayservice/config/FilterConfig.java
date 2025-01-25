77b4c144934b642497f5369d66fd60477c39c38a
Hosung <an3735297@naver.com>
2025-01-25 16:39:46 +0900

    gateway 에 custom filter 적용

--- api-gatewayservice/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
+++ api-gatewayservice/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -5,23 +5,45 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class FilterConfig {
-//    @Bean
+
+    private final CustomFilter customFilter;
+
+    public FilterConfig(CustomFilter customFilter) {
+        this.customFilter = customFilter;
+    }
+
+
+    // custom filter 적용
+    @Bean
     public RouteLocator gatewayLocator(RouteLocatorBuilder locatorBuilder) {
         return locatorBuilder.routes()
                 .route(r -> r.path("/first-service/**")
-                        .filters(f ->
-                                f.addRequestHeader("first-request", "first-request-header")
-                                .addResponseHeader("first-response", "first-response-header")
-                        )
+                        .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config())))
                         .uri("http://localhost:8081"))
                 .route(r -> r.path("/second-service/**")
-                        .filters(f ->
-                                f.addRequestHeader("second-request", "second-request-header")
-                                .addResponseHeader("second-response", "second-response-header")
-                        )
+                        .filters(f -> f.filter(customFilter.apply(new CustomFilter.Config())))
                         .uri("http://localhost:8082"))
                 .build();
     }
+
+
+//    @Bean
+//    public RouteLocator gatewayLocator(RouteLocatorBuilder locatorBuilder) {
+//        return locatorBuilder.routes()
+//                .route(r -> r.path("/first-service/**")
+//                        .filters(f ->
+//                                f.addRequestHeader("first-request", "first-request-header")
+//                                .addResponseHeader("first-response", "first-response-header")
+//                        )
+//                        .uri("http://localhost:8081"))
+//                .route(r -> r.path("/second-service/**")
+//                        .filters(f ->
+//                                f.addRequestHeader("second-request", "second-request-header")
+//                                .addResponseHeader("second-response", "second-response-header")
+//                        )
+//                        .uri("http://localhost:8082"))
+//                .build();
+//    }
 }
